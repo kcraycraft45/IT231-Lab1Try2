723e607300a3aec028e4c984499c3bcfaa2b4dfb
kcraycraft45 <kcraycraft45@southhills.edu>
2024-12-06 13:40:11 -0500

    main6

---
+++ Try2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Try2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Gimme a number! ");
+            int x = int.Parse(Console.ReadLine()!);
+            Console.WriteLine("Anotha one ");
+            int y = int.Parse(Console.ReadLine()!);
+            Console.WriteLine("Thank you");
+            DoMath(x, y);
+        }
+        public static void DoMath(int x, int y)
+        {
+            int z = x + y;
+            Console.WriteLine($"{x} + {y} = {z}");
+        }
+    }
+}
+
